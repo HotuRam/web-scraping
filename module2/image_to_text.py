0405106bd116a5173ffcf8f5fc168a02238dc08c
Hotu <hotugoyal@gmail.com>
2021-09-19 13:35:29 +0530

    added text_showing_image

--- module2/image_to_text.py
+++ module2/image_to_text.py
@@ -2,15 +2,27 @@
 import easyocr
 import cv2
 
-image = cv2.imread('14.png')      #give the path of google ad
-window_name = 'image'          
-cv2.imshow('image',image)
+path = r'C:\Users\my-pc\Desktop\anaconda\hotu_data_scientist_resume\AdEase -ML-Task\web_page_scraping\images2\13.png'
   
-cv2.waitKey(0)        #(this is necessary to avoid Python kernel form crashing)
-cv2.destroyAllWindows()    #closing all open windows 
+# Reading an image in default mode
+image = cv2.imread(path)
+  
+# Window name in which image is displayed
+window_name = 'image'
+  
+# Using cv2.imshow() method 
+# Displaying the image 
+cv2.imshow(window_name, image)
+  
+#waits for user to press any key 
+#(this is necessary to avoid Python kernel form crashing)
+cv2.waitKey(0) 
+  
+#closing all open windows 
+cv2.destroyAllWindows() 
 
 reader  = easyocr.Reader(['en'])
-output = reader.readtext('14.png')          #give the path of google ad.
+output = reader.readtext(path)         
 
 output
 print(output)
